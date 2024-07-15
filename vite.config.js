9dd946737bb92e4873fece1e50b07bd074379be2
zeenat <zeenu.23@gmail.com>
2024-07-15 08:39:20 +0530

    remove base URL and rollupOptions from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const base = '/currency-converter-application/';
-
 export default defineConfig({
   plugins: [react()],
-  base,
   build: {
     outDir: 'dist',
     assetsDir: '',
-    rollupOptions: {
-      input: '/index.html', 
-    },
   },
 });
